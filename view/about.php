8a6edf125d9949d725c71d3d7c4f6d177185bf7d
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2020-12-12 23:26:55 +0700

    Create About.html
    FontAwesome ga jalan

--- view/about.php
+++ view/about.php
@@ -1,6 +1,1 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <script src="./js/navbar.js" defer></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./css/mainStyle.css">
-testtt
+test
