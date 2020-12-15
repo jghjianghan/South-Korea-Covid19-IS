d4ab382e31fc698944ae8f562a9df926b3a55063
crafen <cravensaputra@gmail.com>
2020-12-15 16:05:53 +0700

    Creating Table SQL with MySQL

---
+++ Database/QueryCreateTable/QueryCreateTable.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Admin(
+	idAdmin int NOT NULL AUTO_INCREMENT,
+	username varchar(20) NOT NULL,
+	password varchar(20) NOT NULL,
+	PRIMARY KEY(idAdmin)
+);
+
+CREATE TABLE Time(
+	date DATE,
+	confirmed int,
+	released int,
+	deceased int
+);
+
+CREATE TABLE TimeProvince(
+	date DATE,
+	province_name varchar(20),
+	confirmed int,
+	released int,
+	deceased int,
+);
