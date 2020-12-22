eb7173a06ce6fea424e12c510e6e306fc3be345c
Jiang Han <jianghan99@gmail.com>
2020-12-22 14:33:39 +0700

    Menambah AJAX Endpoint di router

--- router.php
+++ router.php
@@ -2,6 +2,8 @@
 date_default_timezone_set("Asia/Bangkok");
 $url = $_SERVER['REDIRECT_URL'];
 $baseURL = '/South-Korea-Covid19-IS';
+$uri_path = parse_url($url, PHP_URL_PATH);
+$uri_segments = explode('/', $uri_path);
 
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
@@ -46,6 +48,13 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 			require_once "controller/mainController.php";
 			$ctrl = new MainController();
 			echo $ctrl->viewAbout();
+			break;
+
+		case $baseURL . '/data/overall':
+			echo "overall";
+			break;
+		case $baseURL . '/data/regional':
+			echo (isset($_GET["province"])?$_GET["province"]:"regional");
 			break;
 
 		default:
