80ea96eb961567c7a4184660bc0eed5a15ed1f09
vickyricardo <vickyricardo@users.noreply.github.com>
2020-12-23 10:15:26 +0700

    Create getDataRegion function

--- controller/tabelController.php
+++ controller/tabelController.php
@@ -5,7 +5,10 @@
     require_once "model/tabel.php";
 
     class tabelController extends Controller{
-        
+        /**
+         * Method untuk mengambil data overall dari database
+         * @return array data overall
+         */
         public function getDataOverall(){
             $currentCase = 0;
             $query = "
@@ -17,6 +20,29 @@
             $result = [];
 
             foreach($query_result as $key => $value){
+                $result [] = new Tabel($value['date'],$value['confirmed']-$currentCase,$value['confirmed'],$value['released'],$value['deceased']);
+                $currentCase = $value['confirmed'];
+            }
+            return $result;
+        }
+        /**
+         * Method untuk mengambil data region dari database
+         * @param string $region
+         * @return array data region
+         */
+        public function getDataRegion($region)
+        {
+            $currentCase = 0;
+            $query = "
+                SELECT date, confirmed, released, deceased 
+                FROM timeprovince 
+                WHERE province_name='$region'
+            ";
+            $query_result = $this->db->executeSelectQuery($query);
+
+            $result = [];
+
+            foreach ($query_result as $key => $value) {
                 $result [] = new Tabel($value['date'],$value['confirmed']-$currentCase,$value['confirmed'],$value['released'],$value['deceased']);
                 $currentCase = $value['confirmed'];
             }
