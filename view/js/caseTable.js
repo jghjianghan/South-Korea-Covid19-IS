3d7eaa82984b025eecf22d9ab1259a9bd295681f
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2020-12-27 21:19:17 +0700

    sort table (unfinished)

--- view/js/caseTable.js
+++ view/js/caseTable.js
@@ -4,11 +4,11 @@ class CaseTable {
         this.tableHead = this.table.querySelector("thead");
         this.tableBody = this.table.querySelector("tbody");
 
-        console.log(this.table);
         //add event listener to all dropdown option
         let links = document.querySelectorAll("li.dropdown-item");
         for (let i of links) {
             i.addEventListener("click", this.sortEntry);
+            console.log(i.id);
         }
         this.entries = [];
 
@@ -79,7 +79,115 @@ class CaseTable {
      * @param {Event} event
      */
     sortEntry(event) {
-        console.log(this.table);
+        this.table = document.getElementById("case-table");
+        this.tableBody = this.table.querySelector("tbody");
+
+         //data dummy
+         let sortArray = [
+             {
+            date: "date",
+            newCase: "20",
+            confirmed: "2",
+            released: "3",
+            deceased: "4",
+        },
+ {
+            date: "datf0",
+            newCase: "35",
+            confirmed: "25",
+            released: "2",
+            deceased: "4",
+        },
+   {
+            date: "datf1",
+            newCase: "14",
+            confirmed: "10",
+            released: "7",
+            deceased: "9",
+        },
+   {
+            date: "datf2",
+            newCase: "6",
+            confirmed: "9",
+            released: "5",
+            deceased: "4",
+        },
+        {
+            date: "datf3",
+            newCase: "8",
+            confirmed: "22",
+            released: "9",
+            deceased: "3",
+        }];
+
+
+
+        //Sorting
+        let sortId = event.target.id;
+
+        switch(sortId) {
+            case "dateA":
+                sortArray.sort((a, b) => {
+                    return a.date - b.date;
+                });
+              break;
+            case "dateD":
+                sortArray.sort((a, b) => {
+                return b.date - a.date;
+                });
+                break;
+            case "newA":
+                sortArray.sort((a, b) => {
+                    return a.newCase - b.newCase;
+                });
+                break;
+            case "newD":
+                sortArray.sort((a, b) => {
+                    return b.newCase - a.newCase;
+                    });
+                break;
+            case "conA":
+                sortArray.sort((a, b) => {
+                    return a.confirmed - b.confirmed;
+                });
+                break;
+            case "conD":
+                sortArray.sort((a, b) => {
+                    return b.confirmed - a.confirmed;
+                    });
+                break;
+            case "relA":
+                sortArray.sort((a, b) => {
+                    return a.released - b.released;
+                });
+                break;
+            case "relD":
+                sortArray.sort((a, b) => {
+                    return b.released - a.released;
+                    });
+                break;
+            case "decA":
+                sortArray.sort((a, b) => {
+                    return a.deceased - b.deceased;
+                });
+                break;
+            case "decD":
+                sortArray.sort((a, b) => {
+                    return b.deceased - a.deceased;
+                });
+                break;
+            default:
+                sortArray.sort((a, b) => {
+                    return a.date - b.date;
+                });
+          }
+         
+          //show entries on table
+          //cleartable?
+          for(let i=0;i<sortArray.length;i++){
+            let data = new TableEntry(sortArray[i]);
+            this.tableBody.appendChild(data.renderRow());
+          }
     }
 
     /**
