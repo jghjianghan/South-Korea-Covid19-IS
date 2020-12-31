f6e0816af395cb0e8ccad9d1014c259271cc0b78
mariqpratama <ariqpratama25@gmail.com>
2020-12-31 14:14:19 +0700

    add hide/show input password di page login, changePassword, addAccount

--- view/changePassword.php
+++ view/changePassword.php
@@ -7,13 +7,15 @@
                     <input type="password" class="form-control" name="oldPassword" id="oldPassword" style="border: none; border-bottom: 1px solid black; border-radius: 0" placeholder="Old Password">
                     <label for="floatingPassword">Old Password</label>
                 </div>
-                <div class="form-floating my-3">
+                <div class="form-floating input-group-append my-3">
                     <input type="password" class="form-control" name="newPassword" id="newPassword" style="border: none; border-bottom: 1px solid black; border-radius: 0" placeholder="New Password">
                     <label for="floatingPassword">New Password</label>
+                    <span class="mt-3 border-bottom border-dark" id="mybutton" onclick="change('newPassword')"><i class="far fa-eye"></i></span>
                 </div>
-                <div class="form-floating mb-5">
+                <div class="form-floating input-group-append mb-5">
                     <input type="password" class="form-control" name="confirmPassword" id="confirmPassword" style="border: none; border-bottom: 1px solid black; border-radius: 0" placeholder="Confirm Password">
                     <label for="floatingPassword">Confirm Password</label>
+                    <span class="mt-3 border-bottom border-dark" id="mybutton" onclick="change('confirmPassword')"><i class="far fa-eye"></i></span>
                 </div>
                 <div class="form-group mb-3 text-danger">
                     <?php echo $message; ?>
--- view/js/adminScript.js
+++ view/js/adminScript.js
@@ -1,3 +1,15 @@
+/* HIDE/SHOW PASSWORD */
+function change(password) {
+    var x = document.getElementById(password).type;
+
+    if (x == 'password') {
+        document.getElementById(password).type = 'text';
+        document.getElementById('mybutton').innerHTML = '<i class="far fa-eye-slash"></i>';
+    } else {
+        document.getElementById(password).type = 'password';
+        document.getElementById('mybutton').innerHTML = '<i class="far fa-eye"></i>';
+    }
+}
 // function openContent(obj, idContentContainer) {
 //     var i, x, tablinks;
 
