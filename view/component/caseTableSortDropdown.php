db1449ba04109804e5f1da3ec8ce608379a4f986
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2021-01-06 12:24:48 +0700

    change sort icon

--- view/component/caseTableSortDropdown.php
+++ view/component/caseTableSortDropdown.php
@@ -1,6 +1,6 @@
 <div class="dropdown">
-    <button class="btn btn-outline-secondary btn-hover-darkgrey dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
-        Sort
+    <button class="btn btn-outline-secondary btn-hover-darkgrey" type="button" id="dropdownMenu1" data-toggle="dropdown">
+        Sort <i class="fas fa-sort-amount-up-alt"></i>
     </button>
     <ul class="dropdown-menu multi-level">
         <?php
@@ -12,8 +12,8 @@
             <li class="dropdown-submenu">
                 <p class="dropdown-item" tabindex="-1"><?php echo $columnLabels[$i];?></p>
                 <ul class="dropdown-menu">
-                    <li class="dropdown-item sortLink" data-column="<?php echo $tableColumns[$i];?>" data-order="asc">Ascending<i class="fas fa-arrow-up"></i></li>
-                    <li class="dropdown-item sortLink" data-column="<?php echo $tableColumns[$i];?>" data-order="desc">Descending <i class="fas fa-arrow-down"></i></li>
+                    <li class="dropdown-item sortLink" data-column="<?php echo $tableColumns[$i];?>" data-order="asc">Ascending <i class="fas fa-sort-amount-up-alt"></i></li>
+                    <li class="dropdown-item sortLink" data-column="<?php echo $tableColumns[$i];?>" data-order="desc">Descending <i class="fas fa-sort-amount-down-alt"></i></li>
                 </ul>
             </li>
         <?php
